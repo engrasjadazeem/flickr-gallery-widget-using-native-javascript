b9a0bda7e1e48f1758e58c57ec3c1c477461791a
engrasjadazeem <engrasjadazeem@gmail.com>
2017-11-02 14:25:32 +0200

    Update thumbnails

--- assets/script/flickrWidget.js
+++ assets/script/flickrWidget.js
@@ -6,11 +6,37 @@ function invokeFlickrWidget(divId, options) {
 
     //console.log('Invoke flickr widget:'+divId);
     //var divId = 'flickrWidget';
+    
+    //Async GET Request
+    //Type Description
+    //0 -> Initialize
+    //1 -> Load only one image
+    //2 -> Load more
+    fetchFromFlickrAPI(divId, options.thumbnailSize, 0);
+
+    //Remove at end
+    ////Apply thumbnail size
+    //if (options.thumbnailSize !== undefined) {
+    //    applyThumbnailSize(divId, options.thumbnailSize);
+    //}    
+}
+
+
+//Flickr API
+function fetchFromFlickrAPI(divId, thumbnailSize, actionType) {
+
     var widget = document.getElementById(divId);
     var visibleImagesCount = widget.dataset.nbrImage;
     var imagesTopic = widget.dataset.topic;
+    if (thumbnailSize == null) {
+        //Default
+        thumbnailSize = "140px";
+    }
 
-    //Async GET Request
+    //Special case for type "LoadOne"
+    if (actionType == 1)
+        visibleImagesCount = 10; //Keeping 9 extra photos incase the photo we fetched was already deleted by user
+
     var request = new XMLHttpRequest();
     var requestURL = 'https://api.flickr.com/services/rest/?method=flickr.photos.search&api_ke'
                       + 'y=b36822a18ddea0e3bd497a333e3b696d&format=json&nojsonca'
@@ -22,36 +48,48 @@ function invokeFlickrWidget(divId, options) {
         if (request.readyState === 4) {
             // Check if the get was successful.
             if (request.status === 200) {
-                //Success flag
+                //Success flag: Loader for the first time
                 //console.log(request.responseText);
                 var data = JSON.parse(request.responseText);
                 var imageList = data.photos.photo;
 
                 //Deleted Images in particular widget
-                var deletedImages = JSON.parse("[" + localStorage.getItem("Widget_" + divId) + "]");
+                var deletedImages = JSON.parse("[" + localStorage.getItem("Widget_" + divId) + "]");                
 
-                imageList.forEach(function (image) {
+                for (var i = 0; i < imageList.length; i++) {
+                    var image = imageList[i];
+
                     //If image is not deleted by the user anytime
                     if (!deletedImages.includes(image.id)) {
+                        //if(That is image is already not visible)
+                        if (document.querySelector("#" + divId + "_" + image.id) != null) {
+                            continue;
+                        }
+
                         //Sample Image URL : https://farm{farm-id}.staticflickr.com/{server-id}/{id}_{o-secret}_o.(jpg|gif|png)
                         var imageURL = 'https://farm' + image.farm + '.staticflickr.com/' + image.server + '/' + image.id + '_' + image.secret + '.jpg';
-                        var imageBox = '<div class="container" id="'+ divId +'_'+ image.id +'" >' +
+                        var imageBox = '<div class="container" id="' + divId + '_' + image.id + '" >' +
                           '<img  src="' + imageURL + '" alt="Image" class="image thumbnail">' +
                           '<div class="overlay" >' +
-                            '<div class="text cursor-pointer" onclick="deleteImage(\''+ divId +'_' + image.id + '\', \'' + divId + '\')"><i class="fa fa-trash fa-lg"></i></div>' +
+                            '<div class="text cursor-pointer" onclick="deleteImage(\'' + divId + '_' + image.id + '\', \'' + divId + '\', \'' + thumbnailSize
+                             + '\')"><i class="fa fa-trash fa-lg"></i></div>' +
                           '</div>' +
                         '</div>';
                         document.getElementById(divId).innerHTML += imageBox;
-                    }
-                });
 
-                //Thumbnail size
-                if (options.thumbnailSize !== undefined) {
-                    var images = document.getElementById(divId).querySelectorAll(".thumbnail");
-                    for (var i = 0; i < images.length; i++) {
-                        images[i].style.width = options.thumbnailSize;
-                        images[i].style.height = options.thumbnailSize;
+                        
+                        if(actionType == 1){
+                            //1 -> Load One
+                            //Add one Image that is not deleted and break loop
+                            break;
+                        }                                                                        
                     }
+                    
+                }
+
+                //Apply thumbnail size
+                if (thumbnailSize !== undefined) {
+                    applyThumbnailSize(divId, thumbnailSize);
                 }
 
             } else {
@@ -68,7 +106,6 @@ function invokeFlickrWidget(divId, options) {
 
     request.send(null);
 }
-
 
 //Appearance
 function applyConfiguration(divId, options) {
@@ -89,16 +126,32 @@ function applyConfiguration(divId, options) {
     }   
 }
 
+function applyThumbnailSize(divId, thumbnailSize) {
+    //Thumbnail size    
+    var images = document.getElementById(divId).querySelectorAll(".thumbnail");
+    for (var i = 0; i < images.length; i++) {
+        images[i].style.width = thumbnailSize;
+        images[i].style.height = thumbnailSize;
+    }    
+}
+
 //Delete Image handler
-function deleteImage(imageRef, widgetId) {
+function deleteImage(imageRef, divId, thumbnailSize) {
     //console.log('Delete Image Id  = ' + imageRef);
     var deleteImageDiv = document.getElementById(imageRef);
     deleteImageDiv.parentNode.removeChild(deleteImageDiv);
 
     //Add to local storage
-    setDeletedImage(imageRef, widgetId);
-}
+    setDeletedImage(imageRef.split('_')[1], divId);
 
+    //Fetch a new image to replace deleted image
+    fetchFromFlickrAPI(divId, thumbnailSize, 1);
+
+    //Remove at end
+    //if (thumbnailSize !== undefined) {
+    //    applyThumbnailSize(divId, thumbnailSize);
+    //}
+}
 
 //Local Storage setter
 function setDeletedImage(imageId, widgetId) {
@@ -118,4 +171,46 @@ function setDeletedImage(imageId, widgetId) {
     else {
         document.getElementById("ErrorBrowser").innerHTML = "Sorry, your browser does not support Web Storage...";
     }
+}
+
+//Widget scroller
+function WidgetScrolled(divId) {
+    var myDiv = document.getElementById(divId);
+    if (myDiv.offsetHeight + myDiv.scrollTop >= myDiv.scrollHeight) {
+        //scrolledToBottom(e);
+        console.log("Touched ground!");
+
+        //Block UI
+        loadBlockUI(divId);
+
+        //Default
+        var thumbnailSize = "140px";
+        if (document.getElementById(divId).querySelector(".thumbnail") != null) {
+            thumbnailSize = document.getElementById(divId).querySelector(".thumbnail").style.width;
+        }
+
+        //Load more images event call
+        fetchFromFlickrAPI(divId, thumbnailSize, 2);
+
+    } else {
+        console.log("Not yet!");        
+    }
+}
+
+//Block UI
+function loadBlockUI(divId) {
+    var blockUI = document.createElement("div");
+    blockUI.style = 'width: inherit; height: inherit; background-color: black; z-index: +999; position: fixed; opacity: 0.7;'
+    blockUI.setAttribute("id", divId + "_" + "blocker");
+
+    //Center space
+    var positionTop = document.getElementById(divId).style.height.replace(/[^-\d\.]/g, '') - 50;
+    var positionLeft = document.getElementById(divId).style.width.replace(/[^-\d\.]/g, '')/2 - 50;
+
+    blockUI.innerHTML = '<div><i style="color:#ffffff; position:fixed; top:' + positionTop + 'px; left:' + positionLeft + 'px;" class="fa fa-spinner fa-spin fa-3x fa-fw"></i></div>'
+    document.getElementById(divId).appendChild(blockUI);    
+}
+
+function unloadBlockUI(divId) {
+    document.getElementById(divId + "_" + "blocker").style.display = "none";
 }
